19372013df96a5dee6b912f2d229be0f106b6b25
Nicolò Curioni <nicolo.curioni96@gmail.com>
2025-03-06 00:36:08 +0200

    Added the content of the first video tutorial - How to Code in Swift

---
+++ LearnSwift6.playground/Pages/HowToCode.xcplaygroundpage/Contents.swift
@@ -0,0 +1,64 @@
+import SwiftUI
+
+// 👉 How to Code in Swift
+//
+// ### Learn Swift 6 - How to code in Swift #1
+
+var fullName = "Tim Cook"
+var totalUSEStates: Int = 49
+
+let mascotName: String = "Mia 🐶"
+
+    
+    // Variable
+    fullName = "Apple CEO, Tim Cook"
+    totalUSEStates = 50
+    
+    // Constant
+    //mascotName = "Mario Rossi"
+    
+    print(mascotName + " is awesome!")
+    print("Total USA 🇺🇸 states: \(totalUSEStates)" + "\n")
+    
+
+/*
+ HOW IT WORKS
+ 
+ // 1) Bla bla
+ // 2) Start the timer
+ // 3) Run the ML Shower Logic
+ // 4) ...
+ 
+ */
+
+/*
+ Naming Convetions
+ 
+ What we can do?
+ 
+ var userAge = 29
+ var firstUserAge1 = 29
+ var 🇧🇬 = "Bulgaria"
+ 
+ for magicNumber in numbers {
+ 
+    print(magicNumber)
+ }
+ 
+ ---
+ 
+ What we can't do?
+ 
+ var x = 123
+ var ? = "Bla bla"
+ 
+ var numbers = [1, 2, 3]
+ 
+ for n in numbers {
+ 
+    print(n)
+ }
+ 
+ 
+ */
+    
